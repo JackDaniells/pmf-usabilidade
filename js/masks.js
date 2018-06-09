e9f60c646f1012b445d988fdd7be27f426154b35
Matheus Laureano <matheuslaureanol1@gmail.com>
2018-06-09 10:23:58 -0300

    Negativa gera apenas se CPF >= 11 e arrumei um pouco a responsividade

--- js/masks.js
+++ js/masks.js
@@ -1,6 +1,5 @@
  cpfMask = function () {
     var v = $("#cpf").val();
-    console.log('Entrou ', v);
     v = v.replace(/\D/g, "")
     v = v.replace(/(\d{3})(\d)/, "$1.$2");
     v = v.replace(/(\d{3})(\d)/, "$1.$2");
