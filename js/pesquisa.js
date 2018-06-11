8f4d62c11305abca50c4d772ea5d27ac3f54e7e4
daniel <danielk.kock@gmail.com>
2018-06-10 21:54:14 -0300

    alterado metodo de pesquisa pra ira pra tela de pavimentacao de rua tambem

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -1,10 +1,20 @@
 pesquisar = function () {
     var texto = $('#pesquisa').val();
 
-    (texto.includes('negat') || texto.includes('débi') || texto.includes('deb'))
-        ? window.open("./pages/negativa-debito.html", "_self")
-        : texto.includes('consul') ? window.open("./pages/consultas.html", "_self")
-            : texto.includes('solici') ? window.open("./pages/solicitacao.html", "_self")
-                : texto.includes('info') ? window.open("./pages/informacoes.html", "_self")
-                    : console.error('Pesquisa não reconhecida');
+    //negativa de debito
+    if (texto.includes('negat') || texto.includes('débi') || texto.includes('deb')) {
+      window.open("./pages/negativa-debito.html", "_self")
+      //pavimentação de rua
+    } else if (texto.includes('pav') || texto.includes('rua')) {
+        window.open("./pages/pavimentacao-rua.html", "_self")
+        //tela de consulta
+    } else if(texto.includes('consul')) {
+      window.open("./pages/consultas.html", "_self")
+      //tela de solicitação
+    } else if(texto.includes('solici')) {
+      window.open("./pages/solicitacao.html", "_self")
+      // nenhuma tela encontrada
+    } else {
+      alert('Nenhum serviço encontrado');
+    }
 };
